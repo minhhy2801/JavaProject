11bce0d7e69297e9cf609ee73addb7614c545ea7
ToanTLM <37370884+ToanTLM@users.noreply.github.com>
2018-04-01 12:20:16 +0700

    Update Ex4.java

--- Ex4.java
+++ Ex4.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Using Two Arrays save information Fullname and mark of Student 
 package javaapplication14;
 
 import java.util.*;
