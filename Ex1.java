cdaa83cf3c56c49ee6fa2cc8af1ec0bee75c8195
minhhy2801 <34290130+minhhy2801@users.noreply.github.com>
2018-04-01 12:46:57 +0700

    Sửa bài tập 1

--- Ex1.java
+++ Ex1.java
@@ -1,8 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Nhấp Ctrl + Shift + F để clean code
+
+
 //Input an integer n, the program will show the first n primes 
 package javaapplication10;
 
@@ -16,14 +14,12 @@ public class Ex1 {
 
 public static void main(String[]args)
     {
-        int count = 0, max = 0, i;
-        Scanner in = new Scanner(System.in);
-        max = in.nextInt();
-        System.out.println("First "+max+" Prime Numbers:");
+        int count = 0, max_count = 100, i;  //max count gì đấy?
+        System.out.println("First "+max_count+" Prime Numbers:");
 
-        for(int num=1; count<max; num++)
+        for(int num=1; count<max_count; num++)
         {
-            for(i=2; num%i != 0; i++);
+            for(i=2; num%i != 0; i++); //num % i != 0 không hiểu =)))
 
             if(i == num)
             {
